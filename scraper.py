cc5961407da055b8d03bc5c5da4b594d81a43f0a
altcaelum <59708218+altcaelum@users.noreply.github.com>
2026-05-27 01:57:53 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -2,37 +2,50 @@ from playwright.sync_api import sync_playwright
 from feedgen.feed import FeedGenerator
 from datetime import datetime
 
-sites=[
-{"name":"deziiign","url":"https://deziiign.com/","feed":"deziiign.xml"},
-{"name":"mediiia","url":"https://mediiia.com/","feed":"mediiia.xml"}
+sites = [
+    {
+        "name": "deziiign",
+        "url": "https://deziiign.com/",
+        "feed": "deziiign.xml"
+    },
+    {
+        "name": "mediiia",
+        "url": "https://mediiia.com/",
+        "feed": "mediiia.xml"
+    }
 ]
 
 with sync_playwright() as p:
-    browser=p.chromium.launch()
-    page=browser.new_page()
+    browser = p.chromium.launch()
+    page = browser.new_page()
 
     for s in sites:
-        page.goto(s["url"], wait_until="domcontentloaded", timeout=60000)
-page.wait_for_timeout(5000)
+        page.goto(
+            s["url"],
+            wait_until="domcontentloaded",
+            timeout=60000
+        )
 
-        links=page.locator("a").evaluate_all(
-        '''els=>els.map(e=>({
-        title:e.innerText,
-        href:e.href
-        })).filter(x=>x.title&&x.href)'''
-        )[:20]
+        page.wait_for_timeout(5000)
 
-        fg=FeedGenerator()
+        links = page.locator("a").evaluate_all("""
+            els => els.map(e => ({
+                title: e.innerText,
+                href: e.href
+            })).filter(x => x.title && x.href)
+        """)[:20]
+
+        fg = FeedGenerator()
         fg.title(s["name"])
         fg.link(href=s["url"])
         fg.description(f"Generated feed for {s['name']}")
 
-        for i in links:
-            e=fg.add_entry()
-            e.title(i["title"])
-            e.link(href=i["href"])
-            e.guid(i["href"])
-            e.pubDate(datetime.utcnow())
+        for item in links:
+            entry = fg.add_entry()
+            entry.title(item["title"])
+            entry.link(href=item["href"])
+            entry.guid(item["href"])
+            entry.pubDate(datetime.utcnow())
 
         fg.rss_file(s["feed"])
 
