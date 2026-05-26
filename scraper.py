4d4f4255d9372bfc59764c807b368694bcf4705d
altcaelum <59708218+altcaelum@users.noreply.github.com>
2026-05-27 02:14:47 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -29,18 +29,53 @@ with sync_playwright() as p:
         page.wait_for_timeout(5000)
 
         links = page.locator("a").evaluate_all("""
-            els => els.map(e => ({
-                title: e.innerText,
-                href: e.href
-            })).filter(x => x.title && x.href)
-        """)[:20]
+        els => els
+        .map(e => ({
+            title: e.innerText.trim(),
+            href: e.href
+        }))
+        .filter(x =>
+            x.title &&
+            x.href &&
+            x.title.length > 10 &&
+
+            ![
+                "tags",
+                "blogs",
+                "awards",
+                "residents",
+                "education",
+                "competencies",
+                "contact us",
+                "marketplace",
+                "dafes awards",
+                "magistracy",
+                "themes"
+            ].includes(x.title.toLowerCase()) &&
+
+            !x.href.includes("/tags") &&
+            !x.href.includes("/residents") &&
+            !x.href.includes("/awards") &&
+            !x.href.includes("/education") &&
+            !x.href.includes("/marketplace") &&
+            !x.href.includes("/themes") &&
+            !x.href.includes("/magistracy")
+        )
+        """)[:30]
 
         fg = FeedGenerator()
         fg.title(s["name"])
         fg.link(href=s["url"])
         fg.description(f"Generated feed for {s['name']}")
 
+        seen = set()
+
         for item in links:
+            if item["href"] in seen:
+                continue
+
+            seen.add(item["href"])
+
             entry = fg.add_entry()
             entry.title(item["title"])
             entry.link(href=item["href"])
